f080a916e830fd6490b48a3f1e7d8c346c0d678f
Johan Echeverry <jmecheverry90@gmail.com>
2024-07-22 21:34:58 -0500

    Hicimos un formulario validado con JavaScript

---
+++ script.js
@@ -0,0 +1,11 @@
+const array = ['HTML', 'CSS', 'JS', 'ReactJS'];
+
+const object = {nombre: 'Johan Echeverry', ocupacion: 'Desarrollador', materia: 'JavaScript para DOM'}   // Este es un objeto literal
+
+const objectArray = [   //Array de objetos literales
+    { materia: 'HTML', difucultad: 6, necesario: true}, //Objeto Literal 
+    { materia: 'CSS', difucultad: 8, necesario: true}, //Objeto Literal
+    { materia: 'JS', difucultad: 9.5, necesario: false}, //Objeto Literal
+    { materia: 'ReactJS', difucultad: 8, necesario: false}, //Objeto Literal
+    
+]
